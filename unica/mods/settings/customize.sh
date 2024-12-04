06fc42b2a731a74a870e1d99f1a13f9faeb2e8f9
alnoman13nxt <alnoman13nxt@gmail.com>
2024-12-04 20:59:44 +0600

    Remove build prop add

--- unica/mods/settings/customize.sh
+++ unica/mods/settings/customize.sh
@@ -36,8 +36,6 @@ SET_PROP()
 }
 # ]
 
-SET_PROP "ro.unica.version" "$ROM_VERSION" "$WORK_DIR/system/system/build.prop"
-SET_PROP "ro.unica.codename" "$ROM_CODENAME" "$WORK_DIR/system/system/build.prop"
 
 if [ ! -d "$APKTOOL_DIR/system/priv-app/SecSettings/SecSettings.apk" ]; then
     bash "$SRC_DIR/scripts/apktool.sh" d "/system/priv-app/SecSettings/SecSettings.apk"
